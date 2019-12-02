e47ae00a9036c68531ef86b1c17f91a8d686958f
Hoover <thoover1@uab.edu>
2019-12-02 17:08:15 -0600

    thomas made this branch! Ahoy!

--- 12days.js
+++ 12days.js
@@ -1,14 +1,16 @@
 const tweleveDaysOfChristmas = {
-    day1: "",
-    day2: "",
-    day3: "",
-    day4: "",
-    day5: "",
-    day6: "",
-    day7: "",
-    day8: "",
-    day9: "",
-    day10: "",
-    day11: "",
-    day12: ""
-}
+  day1: "",
+  day2: "",
+  day3: "",
+  day4: "",
+  day5: "",
+  day6: "",
+  day7: "",
+  day8: "",
+  day9: "",
+  day10: "",
+  day11: "",
+  day12: ""
+};
+
+/////////////////// I changed thingsssssssssssss
