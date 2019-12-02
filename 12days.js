d57439687cd6a41ef49bc9ac0b86817a3ddf0cea
Joely Vernier <joelyvernier@Joelys-MacBook-Air.local>
2019-12-02 16:23:04 -0700

    Finished the days

--- 12days.js
+++ 12days.js
@@ -1,14 +1,14 @@
 const tweleveDaysOfChristmas = {
     day1: "A bird in a tree?",
     day2: "Pipers piping, I think.",
-    day3: "",
+    day3: "Sonic the Hedgehog.",
     day4: "Who the fuck gives someone a bird in a tree as a gift?",
     day5: "Five golden rings!",
-    day6: "",
-    day7: "",
+    day6: "Geese.",
+    day7: "Swans swimming. Little bastards.",
     day8: "I mean seriously, that's kinda ridiculous.",
     day9: "Nine ladies dancing they were dancing for me",
-    day10: "",
+    day10: "10 Lordes leaping.",
     day11: "Eleven pipers piping, ten lords a leaping (Ba ba ba ba ba ba ba oh)",
     day12: "Twelve drummers drumming like Olympus above the Serengeti"
 };
